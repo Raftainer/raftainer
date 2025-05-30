e843bcc4a4ee1795ab6a4713ac209bce7f021932
William Kronmiller <wrk961@gmail.com>
2025-05-30 06:51:43 -0400

    Write to debug log

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,15 @@
 import Pino from "pino";
 
-export const logger = Pino({});
+export const logger = Pino({
+  transport: {
+    targets: [
+      {
+        target: 'pino/file',
+        level: 'debug',
+        options: {
+          destination: './debug.log'
+        }
+      }
+    ]
+  }
+});
